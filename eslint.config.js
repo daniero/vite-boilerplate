8e1c3f456133f2093181f766f1e655f6b9dfbdfb
Daniel Rødskog <daniel@integral.no>
2024-11-15 13:06:52 +0100

    Configure React

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,8 @@
 import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   {
@@ -21,14 +22,21 @@ export default tseslint.config(
         }
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
+    settings: {
+        react: {
+            version: 'detect',
+        }
+    },
     rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+        'error',
       ],
     },
   },
