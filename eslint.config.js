9f6cb6f24b877a30cde3072c0d7b723281a724c1
Daniel Rødskog <daniel@integral.no>
2024-11-19 13:34:01 +0100

    Install Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,26 @@
-import js from '@eslint/js'
-import tseslint from 'typescript-eslint'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default tseslint.config(
   {
-      ignores: ['dist'],
+    ignores: ['dist'],
   },
   {
     extends: [
-        js.configs.recommended,
-        ...tseslint.configs.stylisticTypeChecked,
-        ...tseslint.configs.strictTypeChecked,
+      js.configs.recommended,
+      ...tseslint.configs.stylisticTypeChecked,
+      ...tseslint.configs.strictTypeChecked,
     ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
-        parserOptions: {
-            project: ['./tsconfig.node.json', './tsconfig.app.json'],
-            tsconfigRootDir: import.meta.dirname,
-        }
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     plugins: {
       react,
@@ -27,17 +28,16 @@ export default tseslint.config(
       'react-refresh': reactRefresh,
     },
     settings: {
-        react: {
-            version: 'detect',
-        }
+      react: {
+        version: 'detect',
+      },
     },
     rules: {
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'error',
-      ],
+      'react-refresh/only-export-components': ['error'],
     },
   },
-)
+  prettier,
+);
